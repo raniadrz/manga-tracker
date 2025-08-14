71ebe55b8e3df0e00d8b46988e83dd458333e483
raniadrz <raniadrz@users.noreply.github.com>
2025-08-14 22:27:52 +0300

    remove little of options from addmanga file

--- src/components/AddMangaForm.jsx
+++ src/components/AddMangaForm.jsx
@@ -112,98 +112,22 @@ function AddMangaForm({ onAddManga }) {
               <option value="horror">Horror</option>
               <option value="mystery">Mystery</option>
               <option value="romance">Romance</option>
+              <option value="harem">Harem</option>
               <option value="sci-fi">Sci-Fi</option>
               <option value="slice-of-life">Slice of Life</option>
               <option value="sports">Sports</option>
               <option value="supernatural">Supernatural</option>
-              <option value="thriller">Thriller</option>
-            </optgroup>
-            
-            {/* Sub-genres and Specific Categories */}
-            <optgroup label="Sub-genres">
-              <option value="action-comedy">Action Comedy</option>
-              <option value="action-drama">Action Drama</option>
-              <option value="adventure-fantasy">Adventure Fantasy</option>
-              <option value="comedy-romance">Comedy Romance</option>
-              <option value="dark-fantasy">Dark Fantasy</option>
-              <option value="drama-romance">Drama Romance</option>
-              <option value="fantasy-romance">Fantasy Romance</option>
-              <option value="psychological">Psychological</option>
-              <option value="psychological-thriller">Psychological Thriller</option>
-              <option value="romantic-comedy">Romantic Comedy</option>
-              <option value="supernatural-romance">Supernatural Romance</option>
-            </optgroup>
-            
-            {/* Demographics */}
-            <optgroup label="Demographics">
               <option value="shounen">Shounen</option>
               <option value="shoujo">Shoujo</option>
               <option value="seinen">Seinen</option>
               <option value="josei">Josei</option>
-              <option value="kodomomuke">Kodomomuke</option>
-            </optgroup>
-            
-            {/* Specific Themes */}
-            <optgroup label="Themes">
               <option value="school-life">School Life</option>
               <option value="work-life">Work Life</option>
               <option value="isekai">Isekai</option>
               <option value="reincarnation">Reincarnation</option>
               <option value="time-travel">Time Travel</option>
               <option value="vampire">Vampire</option>
-              <option value="zombie">Zombie</option>
-              <option value="mecha">Mecha</option>
-              <option value="martial-arts">Martial Arts</option>
-              <option value="music">Music</option>
               <option value="cooking">Cooking</option>
-              <option value="gaming">Gaming</option>
-              <option value="virtual-reality">Virtual Reality</option>
-              <option value="cyberpunk">Cyberpunk</option>
-              <option value="steampunk">Steampunk</option>
-              <option value="post-apocalyptic">Post-Apocalyptic</option>
-              <option value="historical">Historical</option>
-              <option value="war">War</option>
-              <option value="military">Military</option>
-              <option value="police">Police</option>
-              <option value="detective">Detective</option>
-              <option value="medical">Medical</option>
-              <option value="legal">Legal</option>
-              <option value="business">Business</option>
-              <option value="politics">Politics</option>
-            </optgroup>
-            
-            {/* Content Types */}
-            <optgroup label="Content Types">
-              <option value="battle-shounen">Battle Shounen</option>
-              <option value="magical-girl">Magical Girl</option>
-              <option value="harem">Harem</option>
-              <option value="reverse-harem">Reverse Harem</option>
-              <option value="yuri">Yuri</option>
-              <option value="yaoi">Yaoi</option>
-              <option value="bl">BL (Boys Love)</option>
-              <option value="gl">GL (Girls Love)</option>
-              <option value="ecchi">Ecchi</option>
-              <option value="hentai">Hentai</option>
-              <option value="gore">Gore</option>
-              <option value="tragedy">Tragedy</option>
-              <option value="parody">Parody</option>
-              <option value="4-koma">4-Koma</option>
-              <option value="anthology">Anthology</option>
-            </optgroup>
-            
-            {/* Special Categories */}
-            <optgroup label="Special Categories">
-              <option value="completed">Completed</option>
-              <option value="ongoing">Ongoing</option>
-              <option value="hiatus">Hiatus</option>
-              <option value="cancelled">Cancelled</option>
-              <option value="one-shot">One Shot</option>
-              <option value="doujinshi">Doujinshi</option>
-              <option value="manhua">Manhua (Chinese)</option>
-              <option value="manhwa">Manhwa (Korean)</option>
-              <option value="webtoon">Webtoon</option>
-              <option value="light-novel">Light Novel</option>
-              <option value="novel">Novel</option>
             </optgroup>
           </select>
         </div>
